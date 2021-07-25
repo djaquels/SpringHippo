d28979081fabecc8370bdab10c8e46c0db60f6d9
daniel <daniel@pop-os.localdomain>
2021-07-24 19:16:28 -0500

    hello gcp

---
+++ src/test/java/cloud/apps/com/Demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.apps.com.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
